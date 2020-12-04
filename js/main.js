9693a54b8e3204f6441a27df35885ecba2f49739
Vasilije Aleksić <vasilije1998@gmail.com>
2020-12-04 19:22:03 +0100

    dan dva

--- js/main.js
+++ js/main.js
@@ -6,29 +6,3 @@ function meni() {
         menu.style.display = "none";
     }
 }
-/*
-function changeLocation(location){
-        window.location.pathname = "EPOS/" + location;
-}*/
-/*
-$(document).ready(function () {
-    var menu = $("#menu")[0];
-    if(window.location.pathname != "/Heineken/index.html"){
-        menu.style.display = "block";
-
-        if(window.location.pathname == "/Heineken/about-us.html"){
-            $("#about-us")[0].firstElementChild.style.color = "white"
-        }
-        else if(window.location.pathname == "/Heineken/contact.html"){
-            $("#contact")[0].nextElementChild.style.color = "white"
-        }
-        else {
-            $("#gallery")[0].nextElementChild.style.color = "white"
-        }
-    }
-
-    else {
-        $("#home")[0].secondtElementChild.style.color = "white"
-    }
-})
-;*/
