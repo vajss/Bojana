4edaab91d888f5e48e402183a06e4632745bac3d
Vasilije Aleksić <vasilije1998@gmail.com>
2020-12-04 21:39:27 +0100

    2 2

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function meni() {
-    var menu = $("#menu")[0];
-    if (menu.style.display === "none" || menu.style.display == '') {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
